c26081abeca268ed80f06b3a2c49c7a93ac4f213
Kenton Roush <kroush@g.clemson.edu>
2016-10-17 12:14:04 -0400

    Created tests for, and implemented, prime number generator.

---
+++ lib/prime_number_generator.rb
@@ -0,0 +1,33 @@
+# lib/prime_number_generator.rb
+
+class PrimeNumberGenerator
+
+  def self.isPrime(num)
+    # Only integers less than the square root of the integer being tested for primality need be checked.
+    # In theory there's any number of ways to speed this up, testing modulus division on only a fraction of even that - Technically, only other primes less than the tested number need included.
+    # ...But the faster the method of isolating primes, the more complex and hard to read it becomes. This isn't a research paper for mathematics, so clarity of code seems a higher priority.
+    max = Math.sqrt(num).ceil
+    
+    for i in 2..max
+      # The second condition here will only ever be true for 2, but handling it this way is technically more accurate to the definition of primality than treating 2 as a special case.
+      if num % i == 0 and num != i then
+        # puts "#{num} is divisible by #{i} - not prime"
+        return false
+      end
+    end
+    return true
+  end
+  
+  def self.generate(first, second)
+    primes = Array.new
+    
+    for i in (first > second ? second..first : first..second)
+      if isPrime(i) then
+        primes.push(i)
+      end
+    end
+    
+    return primes
+  end
+  
+end
---
+++ spec/prime_number_generator_spec.rb
@@ -0,0 +1,88 @@
+require "prime_number_generator"
+
+describe PrimeNumberGenerator do
+
+  
+  describe ".generate" do
+  
+    # Test case: Basic functional test.
+    context "given a range from 2 to 5" do
+      it "returns [2, 3, 5]" do
+        expect(PrimeNumberGenerator.generate(2, 5)).to eql([2, 3, 5])
+      end
+    end
+    
+    # Test case: Basic functional test.
+    context "given a range from 7900 to 7920" do
+      it "returns [7901, 7907, 7919]" do
+        expect(PrimeNumberGenerator.generate(7900, 7920)).to eql([7901, 7907, 7919])
+      end
+    end
+    
+    # Test case: Inverse range - order of inputs should be ignored.
+    context "given a range from 7920 to 7900" do
+      it "returns [7901, 7907, 7919]" do
+        expect(PrimeNumberGenerator.generate(7920, 7900)).to eql([7901, 7907, 7919])
+      end
+    end
+    
+    # Test case: Range containing no prime numbers.
+    context "given a range from 7908 to 7918" do
+      it "returns []" do
+        expect(PrimeNumberGenerator.generate(7908, 7918)).to eql([])
+      end
+    end
+    
+    # Test case: Range containing only prime numbers.
+    context "given a range from 2 to 3" do
+      it "returns [2, 3]" do
+        expect(PrimeNumberGenerator.generate(2, 3)).to eql([2, 3])
+      end
+    end
+    
+    # Test case: Range containing only one number.
+    context "given a range from 5 to 5" do
+      it "returns [5]" do
+        expect(PrimeNumberGenerator.generate(5, 5)).to eql([5])
+      end
+    end
+  end
+  
+  describe ".isPrime" do
+  
+    # Test case: Basic functional tests - Prime.
+    context "given the input 2" do
+      it "returns true" do
+        expect(PrimeNumberGenerator.isPrime(2)).to eql(true)
+      end
+    end
+    context "given the input 5" do
+      it "returns true" do
+        expect(PrimeNumberGenerator.isPrime(2)).to eql(true)
+      end
+    end
+    context "given the input 7919" do
+      it "returns true" do
+        expect(PrimeNumberGenerator.isPrime(2)).to eql(true)
+      end
+    end
+    
+    # Test case: Basic functional tests - Non-Prime.
+    context "given the input 4" do
+      it "returns true" do
+        expect(PrimeNumberGenerator.isPrime(9)).to eql(false)
+      end
+    end
+    context "given the input 7920" do
+      it "returns true" do
+        expect(PrimeNumberGenerator.isPrime(9)).to eql(false)
+      end
+    end
+    context "given the input 1" do
+      it "returns true" do
+        expect(PrimeNumberGenerator.isPrime(9)).to eql(false)
+      end
+    end
+  end
+
+end
